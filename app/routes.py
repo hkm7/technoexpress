68d909ab02aa5c19337c80a371741642c3c805e6
eezo <hrishikeshmohan2000@gmail.com>
2020-04-11 12:40:17 +0530

    More html and routes

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,10 @@ from flask import render_template
 def login():
     return render_template('login.html')
 
-@app.route    
+@app.route('/signup') 
+def signup():
+    return render_template('signup.html')
+
+@app.route('/home') 
+def feed():
+    return render_template('home.html')      
