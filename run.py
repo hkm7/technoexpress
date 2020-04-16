09103c34ea8d85c0f06c047d6e6bbcc7b082a6bd
eezo <hrishikeshmohan2000@gmail.com>
2020-04-16 20:51:50 +0530

    html and database added

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ c= conn.cursor()
 def create_tables():
     c.execute("CREATE TABLE IF NOT EXISTS credentials(uid REAL, uname TEXT, pwd TEXT)")
     c.execute("CREATE TABLE IF NOT EXISTS feedContent(uid REAL, datestamp TEXT, title TEXT, content TEXT)")
-    c.close()
-    conn.close()
-
+   
+c.close()
+conn.close()
     
 if __name__=='__main__':
     create_tables()
